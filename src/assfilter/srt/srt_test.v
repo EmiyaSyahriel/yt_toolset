d13ba6ad3ba9643d1d4247c86ec61c4dd3014c70
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2026-05-11 16:36:03 +0700

    add: srt time parse test

--- src/assfilter/srt/srt_test.v
+++ src/assfilter/srt/srt_test.v
@@ -1,5 +1,19 @@
 module srt
 
+import time
+
+fn hh_mm_ss_mss(h int, m int, s int, ms int) time.Duration {
+	return (h * time.hour) +
+		(m * time.minute) +
+		(s * time.second) +
+		(ms * time.millisecond)
+}
+
 fn test_parse_time() {
-	
+	assert parse_srt_time(0, "05:03:10.100")! == hh_mm_ss_mss(5, 3, 10, 100), "dot-decimal format does not work"
+	assert parse_srt_time(0, "05:03:10,100")! == hh_mm_ss_mss(5, 3, 10, 100), "comma-decimal format does not work"
+	assert parse_srt_time(0, "05:03:10")! == hh_mm_ss_mss(5, 3, 10, 0), "hh:mm:ss does not work"
+	assert parse_srt_time(0, "03:10.100")! == hh_mm_ss_mss(0, 3, 10, 100), "mm:ss.mss does not work"
+	assert parse_srt_time(0, "03:10,100")! == hh_mm_ss_mss(0, 3, 10, 100), "mm:ss,mss does not work"
+	assert parse_srt_time(0, "03:10")! == hh_mm_ss_mss(0, 3, 10, 0), "mm:ss does not work"
 }
