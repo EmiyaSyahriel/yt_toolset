02aa5d450a83da0c64cea2003c6e267da04d20cf
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2026-05-14 15:46:58 +0700

    add: make directory on start

--- src/main.v
+++ src/main.v
@@ -6,6 +6,8 @@ import structurer
 pub fn main()! {
 	mut core_app := core.Core.new()
 
+
+	core_app.mkdir_all_related_directories() or { panic(err) }
 	core_app.register(core.HelpMan.new(core_app))
 	core_app.register(structurer.Structurer.new(core_app))
 
